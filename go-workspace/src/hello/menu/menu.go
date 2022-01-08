b791d798fe629214269927c7a56c98891dce0bf1
Joao Vitor <outlast4321@gmail.com>
2022-01-08 10:52:14 -0300

    starting http requests

--- go-workspace/src/hello/menu/menu.go
+++ go-workspace/src/hello/menu/menu.go
@@ -2,26 +2,30 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 )
 
 func main() {
-	exibeIntro()
-	//switch no go nao precisa de break
-	comando := leComando()
-	switch comando {
-	case 1:
-		fmt.Println("Iniciando monitoramento...")
-	case 2:
-		fmt.Println("Exibindo logs...")
-	case 0:
-		fmt.Println("Encerrando...")
-		os.Exit(0)
-	default:
-		fmt.Println("Comando nao reconhecido! ")
-		os.Exit(-1)
-	}
+	for {
+		exibeIntro()
+		//switch no go nao precisa de break
+		comando := leComando()
+		switch comando {
+		case 1:
+			fmt.Println("Iniciando monitoramento...")
+			iniciarMonitoramento()
+		case 2:
+			fmt.Println("Exibindo logs...")
+		case 0:
+			fmt.Println("Encerrando...")
+			os.Exit(0)
+		default:
+			fmt.Println("Comando nao reconhecido! ")
+			os.Exit(-1)
+		}
 
+	}
 }
 
 func exibeIntro() {
@@ -37,4 +41,15 @@ func leComando() int {
 	fmt.Println("Endereco da variavel comando -> ", &comando) //memoria
 	fmt.Println("O comando escolhido foi", comando)
 	return comando
+}
+func iniciarMonitoramento() {
+	site := "https://github.com"
+	resp, _ := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println("Site:", site, " foi carregado com sucesso!")
+	} else {
+		fmt.Println("Site:", site, " nao foi carregado!")
+		fmt.Println("Status code -> ", resp.StatusCode)
+	}
+
 }
